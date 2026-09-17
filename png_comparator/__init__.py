7a892ed14f992b8d642f4420a0d1d3e00bffb6f3
Nighttug58 <makrobion@gmail.com>
2026-09-17 11:19:03 +0200

    refactor: create png_comparator package

---
+++ png_comparator/__init__.py
@@ -0,0 +1,7 @@
+"""PNG Comparator package.
+
+This package is being extracted progressively from the historical monolithic
+application. Public behavior is intentionally kept unchanged during the split.
+"""
+
+__all__ = []

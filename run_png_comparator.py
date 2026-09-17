03b86e163c26c7a30f1b8981ddde596389dbe37c
Nighttug58 <makrobion@gmail.com>
2026-09-17 11:22:51 +0200

    refactor: wire extracted config and helpers

--- run_png_comparator.py
+++ run_png_comparator.py
@@ -9,9 +9,38 @@ from __future__ import annotations
 
 import png_comparator_v3_01_alpha_cleanup as legacy_app
 
+from png_comparator import config
 from png_comparator.image_cache import ImageMemoryCache
 from png_comparator.models import Annotation, DrawingItem, ImageRecord, ImageScanResult, ViewerState
 from png_comparator.scanner import ImageScanner, extract_tags
+from png_comparator.shortcuts import (
+    SHORTCUT_ALIASES,
+    SHORTCUT_DEFINITIONS,
+    portable_shortcut_text,
+    shortcut_default_preferences,
+)
+from png_comparator.status import (
+    STATUS_ALIASES,
+    STATUS_CHOICES,
+    STATUS_DEFAULT,
+    has_annotation_content,
+    is_default_status,
+    normalize_status,
+    status_text_color,
+)
+from png_comparator.utils import (
+    canonical_image_key,
+    color_to_tuple,
+    format_rgb,
+    human_file_size,
+    image_border_color_for_background,
+    image_metadata_summary_fast,
+    loose_path_basename,
+    open_path_default,
+    reveal_in_file_manager,
+    tuple_to_color,
+    wildcard_text_match,
+)
 
 
 def install_modular_components() -> None:
@@ -21,6 +50,22 @@ def install_modular_components() -> None:
     module au moment de leur exécution. L'injection a donc lieu avant legacy_app.main().
     """
 
+    # Configuration / chemins.
+    legacy_app.APP_NAME = config.APP_NAME
+    legacy_app.CHECKBOX_VISUAL_STYLE = config.CHECKBOX_VISUAL_STYLE
+    legacy_app.APP_INSTALL_DIR = config.APP_INSTALL_DIR
+    legacy_app.APP_ROOT_DIR = config.APP_ROOT_DIR
+    legacy_app.STATE_FILE = config.STATE_FILE
+    legacy_app.SESSION_RECENTS_FILE = config.SESSION_RECENTS_FILE
+    legacy_app.LEGACY_STATE_FILE = config.LEGACY_STATE_FILE
+    legacy_app.AUTOSAVE_INTERVAL_MS = config.AUTOSAVE_INTERVAL_MS
+    legacy_app.MAX_RECENT_SESSIONS = config.MAX_RECENT_SESSIONS
+    legacy_app.MAX_BACKUPS_PER_FILE = config.MAX_BACKUPS_PER_FILE
+    legacy_app.DEFAULT_BG_COLORS = config.DEFAULT_BG_COLORS
+    legacy_app.DEFAULT_PREVIEW_MAX_SIDE = config.DEFAULT_PREVIEW_MAX_SIDE
+    legacy_app.DEFAULT_PRELOAD_RADIUS = config.DEFAULT_PRELOAD_RADIUS
+
+    # Modèles et services cœur.
     legacy_app.ImageRecord = ImageRecord
     legacy_app.Annotation = Annotation
     legacy_app.ViewerState = ViewerState
@@ -29,6 +74,34 @@ def install_modular_components() -> None:
     legacy_app.ImageScanner = ImageScanner
     legacy_app.extract_tags = extract_tags
     legacy_app.ImageMemoryCache = ImageMemoryCache
+
+    # Statuts.
+    legacy_app.STATUS_DEFAULT = STATUS_DEFAULT
+    legacy_app.STATUS_CHOICES = STATUS_CHOICES
+    legacy_app.STATUS_ALIASES = STATUS_ALIASES
+    legacy_app.normalize_status = normalize_status
+    legacy_app.is_default_status = is_default_status
+    legacy_app.status_text_color = status_text_color
+    legacy_app.has_annotation_content = has_annotation_content
+
+    # Raccourcis.
+    legacy_app.SHORTCUT_DEFINITIONS = SHORTCUT_DEFINITIONS
+    legacy_app.SHORTCUT_ALIASES = SHORTCUT_ALIASES
+    legacy_app.shortcut_default_preferences = shortcut_default_preferences
+    legacy_app.portable_shortcut_text = portable_shortcut_text
+
+    # Utilitaires partagés.
+    legacy_app.canonical_image_key = canonical_image_key
+    legacy_app.loose_path_basename = loose_path_basename
+    legacy_app.image_border_color_for_background = image_border_color_for_background
+    legacy_app.wildcard_text_match = wildcard_text_match
+    legacy_app.format_rgb = format_rgb
+    legacy_app.color_to_tuple = color_to_tuple
+    legacy_app.tuple_to_color = tuple_to_color
+    legacy_app.human_file_size = human_file_size
+    legacy_app.image_metadata_summary_fast = image_metadata_summary_fast
+    legacy_app.open_path_default = open_path_default
+    legacy_app.reveal_in_file_manager = reveal_in_file_manager
 
 
 def main() -> int:
